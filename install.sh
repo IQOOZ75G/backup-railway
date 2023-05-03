f69ddb9fc28f4af0fe8c1bfc47845a62dd84087b
Yash Kumar <104177185+Nightzhunterox@users.noreply.github.com>
2023-05-03 23:17:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,9 +19,6 @@ done < <(printf '%s\n' "$list")
 
 
 
-chmod 777 ./ngrok
-./ngrok authtoken $ngrok_authtoken > /dev/null 2>&1
-
 printf "\n\n Instaling.....\n"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - > /dev/null 2>&1
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list > /dev/null 2>&1
